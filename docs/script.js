845a5afa179b041b5c13ee004a289a7339425deb
SegoCode <35817798+SegoCode@users.noreply.github.com>
2021-12-16 16:00:48 +0100

    new format & buffering message

--- docs/script.js
+++ docs/script.js
@@ -1,96 +1,94 @@
-console.log("%cHi there 👋", "color: #36393e; font-size: 30px;");
+console.log('%cHi there 👋', 'color: #36393e; font-size: 30px;');
 console.log("Do you want some music on? Type 'music()'");
-document.getElementById("jsEnabled").style.display = "none";
+document.getElementById('jsEnabled').style.display = 'none';
 
-var lainonRadio = document.getElementById("audio");
+var lainonRadio = document.getElementById('audio');
 var triggerCount = 0; //Key listener
 var dataShowed = false; //Prevent double animation
 
+document.getElementById('hiddenData').addEventListener('click', (event) => {
+	var baffle = window.baffle;
+	var CryptoJS = window.CryptoJS;
+	if (!dataShowed) {
+		dataShowed = true;
+		var baffleElement = baffle(document.querySelectorAll('#hiddenData'));
 
-document.getElementById("hiddenData").addEventListener("click", event => {
-  var baffle = window.baffle;
-  var CryptoJS = window.CryptoJS;
-  if (!dataShowed) {
-    dataShowed = true;
-    var baffleElement = baffle(document.querySelectorAll('#hiddenData'));
-
-    baffleElement.start()
-      .set({
-        speed: 30
-      })
-      .text(text => atob(protected('U2Vnb0NvZGVTZWNyZXRz')))
-      .reveal(1000, 100);
-  }
+		baffleElement
+			.start()
+			.set({
+				speed: 30,
+			})
+			.text((text) => atob(protected('U2Vnb0NvZGVTZWNyZXRz')))
+			.reveal(1000, 100);
+	}
 });
 
-
 function protected(key) {
-  // Scraping protection
-  var bytes = CryptoJS.AES.decrypt('U2FsdGVkX18TpCCHkp0puKKrUoTs+HFOeaze9mRenZ5r/JMtzYPoI7XGxdPH+DQc', atob(key));
-  return bytes.toString(CryptoJS.enc.Utf8);
+	// Scraping protection
+	var bytes = CryptoJS.AES.decrypt('U2FsdGVkX18TpCCHkp0puKKrUoTs+HFOeaze9mRenZ5r/JMtzYPoI7XGxdPH+DQc', atob(key));
+	return bytes.toString(CryptoJS.enc.Utf8);
 }
 
 // Radio code section //
 
 function music() {
-  lainonRadio.volume = 0.3;
+	lainonRadio.volume = 0.3;
 
-  // meyda.min.js //
-  var audioContext = new AudioContext();
-  var source = audioContext.createMediaElementSource(lainonRadio);
-  source.connect(audioContext.destination);
+	// meyda.min.js //
+	var audioContext = new AudioContext();
+	var source = audioContext.createMediaElementSource(lainonRadio);
+	source.connect(audioContext.destination);
 
-  var analyzer = Meyda.createMeydaAnalyzer({
-    "audioContext": audioContext,
-    "source": source,
-    "bufferSize": 512,
-    "featureExtractors": ["rms"],
-    "callback": features => {
-      //background effect
-      //document.body.style.backgroundColor = "hsl(217, 6%, " + 1 + features.rms * 100 + "%)";
+	var analyzer = Meyda.createMeydaAnalyzer({
+		audioContext: audioContext,
+		source: source,
+		bufferSize: 512,
+		featureExtractors: ['rms'],
+		callback: (features) => {
+			var musicLog = document.getElementById('musicLog');
+			document.getElementById('gitname').style.textShadow = '0 0 ' + 3 + features.rms * 100 + 'px #c3c3c3';
 
-      //Cool crazy fonts
-      //document.getElementById('gitname').style.fontSize = 21+features.rms * 100+"px";
-      //document.getElementById('gitname').style.color = "hsl(0, 0%, "+ 1+features.rms * 100 + "%)";
-      document.getElementById('gitname').style.textShadow = "0 0 " + 3 + features.rms * 100 + "px #c3c3c3"
-    }
-  });
-  analyzer.start();
-  // End meyda.min.js //
+			if (features.rms == 0) { //¿Silent?
+				musicLog.textContent = 'CONNECTION ERROR: Buffering...';
+				musicLog.style.display = 'block';
+			} else {
+				musicLog.style.display = 'none';
+			}
+		},
+	});
+	analyzer.start();
+	// End meyda.min.js //
 
-  lainonRadio.play();
-  document.getElementById('volume').className = 'volume active'
-  document.getElementById('volume').textContent = "Volume > " + lainonRadio.volume * 10 + " // scroll to change ";
-  console.log("Playing radio streaming from lainon.life, Buffering...");
-
+	lainonRadio.play();
+	document.getElementById('volume').className = 'volume active';
+	document.getElementById('volume').textContent = 'Volume > ' + lainonRadio.volume * 10 + ' // scroll to change ';
+	console.log('Playing radio streaming from lainon.life, Buffering...');
 }
 
+window.addEventListener('wheel', (event) => {
+	var delta = Math.sign(event.deltaY);
+	var deltaNormalized = delta / -10;
+	var volume = (lainonRadio.volume + deltaNormalized).toFixed(1);
 
-window.addEventListener('wheel', event => {
-  var delta = Math.sign(event.deltaY);
-  var deltaNormalized = delta / -10;
-  var volume = (lainonRadio.volume + deltaNormalized).toFixed(1);
+	if (volume > 1) volume = 1.0;
+	if (volume <= 0) volume = 0.1;
 
-  if (volume > 1) volume = 1.0;
-  if (volume <= 0) volume = 0.1;
-
-  lainonRadio.volume = volume;
-  document.getElementById('volume').textContent = "Volume > " + lainonRadio.volume * 10 + " // scroll to change ";
+	lainonRadio.volume = volume;
+	document.getElementById('volume').textContent = 'Volume > ' + lainonRadio.volume * 10 + ' // scroll to change ';
 });
 
+window.addEventListener('keyup', (event) => {
+	var key = ['m', 'u', 's', 'i', 'c'];
+	var max = key.length;
 
-window.addEventListener('keyup', event => {
-  var key = [77, 85, 83, 73, 67]
-  var max = key.length
+	if (event.key === key[triggerCount]) {
+		triggerCount++;
+	} else {
+		triggerCount = 0;
+	}
 
-  if (event.which === key[triggerCount]) {
-    triggerCount++
-  } else {
-    triggerCount = 0
-  }
-
-  if (triggerCount >= max) {
-    music();
-    triggerCount = 0
-  }
+	if (triggerCount >= max) {
+		music();
+		triggerCount = 0;
+	}
 });
